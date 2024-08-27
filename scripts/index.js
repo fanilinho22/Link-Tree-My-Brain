b73754280dcddc2fdd9726f1bdf39786825ab8dd
fanilinho22 <daniel.pedrozo8266@gmail.com>
2024-08-27 18:16:19 -0300

    CARALHO FDS O MYBRAIN AGORA EH TCS

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,1 @@
-// Função para exibir o aviso
-function exibirAviso() {
-  alert("Esta página está sendo construída ainda.\nEsperamos que você entenda!");
-}
-// Seleciona o último link com o texto "Landing Page" e adiciona um evento de clique
-const landingPageLink = document.getElementById("landing-page-link");
-landingPageLink.addEventListener("click", exibirAviso);
+//nada ainda
